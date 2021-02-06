f5702792cf9a349c9df87c30b6892ea66f043136
shuvo islam <shuvoislamcmt3@gmail.com>
2021-02-06 17:24:42 +0600

    footer makrup

--- footer.php
+++ footer.php
@@ -12,19 +12,19 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'picchi' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'picchi' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'picchi' ), 'picchi', '<a href="https://shuvo.work">Shuvo Islam</a>' );
-				?>
-		</div><!-- .site-info -->
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xxl-12">
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-twitter"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+					<a href=""><i class="fab fa-linkedin-in"></i></a>
+					<p>&copy; All Right Reserve. 2021 </p>
+				</div>
+			</div>
+		</div>
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
